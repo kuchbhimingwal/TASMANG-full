ddf37517b4c59350f2fd9e45d7efa91bef04303c
kuchbhimingwal <shubhammingi@gmail.com>
2024-04-20 09:14:26 -0400

    creted schmema for user and admin and connected to the db with moongoose

---
+++ backend/db/connect.js
@@ -0,0 +1,16 @@
+import mongoose, { mongo } from 'mongoose';
+const userSchema = require("./user");
+const adminSchema = require("./admin")
+mongoose.connect('mongodb+srv://admin:Shubham%4028@cluster0.yzcu80x.mongodb.net/TASMANG');
+
+
+const User = mongoose.model('User', userSchema);
+const Admin = mongoose.model('Admin', adminSchema);
+
+module.exports = {
+  User,
+  Admin
+}
+
+
+
