15b22429ea383f3bd57962cf42f1999ef405e25f
kuchbhimingwal <shubhammingi@gmail.com>
2024-04-23 02:30:02 -0400

    added routing !!!!

--- frontend/taskmng/src/main.jsx
+++ frontend/taskmng/src/main.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Login from '../pages/Login.jsx'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children : [
+      {
+        path: '/login',
+        element: <Login />
+      }
+    ]
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
